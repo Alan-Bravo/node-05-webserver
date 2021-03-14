1f1ec074068e7765f8512d8e56b5938199293e4b
Alan Bravo <bravoalangonzalo@gmail.com>
2021-03-13 22:33:17 -0300

    Primer commit del web server

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const http = require('http');
+require('dotenv').config();
+const express = require('express');
 
-http
-  .createServer((req, res) => {
-    res.write('Hola Mundo');
-    res.end();
-  })
-  .listen(3000);
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.send('Hola Mundo');
+});
+
+app.get('/usuarios', function (req, res) {
+  res.send('Usuario: Alan');
+});
+
+app.post('/', function (req, res) {
+  res.send('POST del Path /');
+});
+
+app.listen(port, () => {
+  console.log(`Server started - Port ${port}`);
+});
